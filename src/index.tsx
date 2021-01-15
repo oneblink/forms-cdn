969397766be1d806428ed2d0e1fecd1720ca59f4
aaron roworth <aaron@oneblink.io>
2021-01-15 16:40:42 +1100

    ON-9718 # added handling for submission

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import '@oneblink/apps-react/dist/styles.css'
 import Form from './form'
+import 'setimmediate'
 
 type Config = {
   formId: number
+  formsAppId: number
   selector: string
+  postSubmissionUrl: string
+  cancelRedirectUrl: string
+  preFillData?: { [key: string]: unknown }
+  externalId?: string
+  googleMapsApiKey?: string
+  captchaSiteKey?: string
 }
 
 export const renderForm = (config: Config) => {
   ReactDOM.render(
     <React.StrictMode>
-      <div>
-        <Form formId={config.formId} />
-      </div>
+      <Form
+        formId={config.formId}
+        formsAppId={config.formsAppId}
+        preFillData={config.preFillData}
+        externalId={config.externalId}
+        googleMapsApiKey={config.googleMapsApiKey}
+        captchaSiteKey={config.captchaSiteKey}
+        postSubmissionUrl={config.postSubmissionUrl}
+        cancelRedirectUrl={config.cancelRedirectUrl}
+      />
     </React.StrictMode>,
     document.getElementById(config.selector),
   )
