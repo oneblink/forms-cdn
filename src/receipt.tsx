6c373e88a372c7d7d099304dd89b15d10529c550
Zac Turner <zac@oneblink.io>
2023-02-14 16:27:19 +1100

    ON-29428 # Added submissionId to query string and added cancel action prop

--- src/receipt.tsx
+++ src/receipt.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 import { PaymentReceipt } from '@oneblink/apps-react'
+import { submissionService } from '@oneblink/apps'
 
 type Props = {
   redirectUrl: string
+  cancelRedirectUrl: string
 }
 
-export default function Receipt({ redirectUrl }: Props) {
-  const onDone = React.useCallback(async () => {
-    window.location.href = redirectUrl
-  }, [redirectUrl])
+export default function Receipt({ redirectUrl, cancelRedirectUrl }: Props) {
+  const onDone = React.useCallback(
+    async (submissionResult: submissionService.FormSubmissionResult) => {
+      const url = new URL(redirectUrl)
+      const searchString = url.search
+        ? `${url.search}&submissionId=${submissionResult.submissionId}`
+        : `?submissionId=${submissionResult.submissionId}`
+      window.location.href = `${url.origin}${searchString}`
+    },
+    [redirectUrl],
+  )
+
+  const handleCancel = React.useCallback(() => {
+    window.location.href = cancelRedirectUrl
+  }, [cancelRedirectUrl])
 
   return (
     <div>
-      <PaymentReceipt onDone={onDone} />
+      <PaymentReceipt onDone={onDone} onCancel={handleCancel} />
     </div>
   )
 }
